e47143fab97c8456b416b7d13b446be605a70f1f
Joe Atkins <joe.atkins@objectpartners.com>
2018-05-22 10:57:39 -0500

    Lab 2 completed successfully

--- lab-02-first-component/src/App.js
+++ lab-02-first-component/src/App.js
@@ -1,10 +1,17 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
+import Hello from './hello/Hello';
 
 class App extends Component {
-  render() {
-    return <div className="App" />;
-  }
+    render() {
+        return (
+            <div className="App">
+
+                <Hello friend="WoooHooo!" />
+
+            </div>
+        );
+    }
 }
 
 export default App;
